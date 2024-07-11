66e6de25a3d4f9f7669193976295636ced16446a
iftikhrlutfur <iftikherlutfur@gmail.com>
2024-07-11 13:47:16 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use(cors())
+
+
+const uri = `mongodb+srv://onlineDukan:UvReBpgeiCOzmFui@cluster0.hyx8zzc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    
+  }
+}
+run().catch(console.dir);
+
+
+
+
 
 app.get('/', (req,res)=>{
     res.send("Jersey Shop is open 24/7")
